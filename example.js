2e1c8c40d17ffd99959d1114ed4f28f141e2545e
contact@danielkorgel.com <264778848eca87c3d106916245f7c66c>
2020-12-25 21:31:27 +0100

    Update JS example

--- example.js
+++ example.js
@@ -1,9 +1,15 @@
-var textAdv = new textAdventureEngine(witeLine);
+var textAdv = new textAdventureEngine(witeLine, clearText);
 // SUBPATH+"/programs/"+game_database)
 textAdv.loadDatabaseFromPath("./lonesurvivor.tbegamedatabase.json");
 
 function witeLine(output){
     console.log("OUT :: "+output);
+    CONSOLE_PREVENT_INPUT = false;
+	BASH_EXECUTING = false;
+}
+
+function clearText(){
+    console.log("CLEAR :: ");
     CONSOLE_PREVENT_INPUT = false;
 	BASH_EXECUTING = false;
 }
