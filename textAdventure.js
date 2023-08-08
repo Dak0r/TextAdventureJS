abe7184b62f8f22d9dbf33035fe47fdd3ff18a47
Daniel Korgel <contact@danielkorgel.com>
2023-08-08 19:57:50 +0200

    Refactored textAdventure.js

--- textAdventure.js
+++ textAdventure.js
@@ -1,15 +1,10 @@
-/*
- * Note: As of 25th of December 2020 private fields are not yet universally supported:
- * --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields
- * Because of this, this class uses an "internal_"-prefix for fields that shold actually be private.
-*/
 class textAdventureEngine {
 	
 	TBA_DEBUG = false;
 
-	internal_Database = undefined;
-	internal_CurrentRoom = null;
-	internal_Inventory = {};
+	#database = undefined;
+	#currentRoom = null;
+	#inventory = {};
 
 	constructor(outputFunction, clearOutputFunction) { 
 		this.outputAddLines = outputFunction;
@@ -22,29 +17,29 @@ class textAdventureEngine {
 		let base = this;
 		$.getJSON( gamedatabasePath)
 		.done(function( json ) {
-			base.internal_initDatbase(json, showGameName);
+			base.#initDatbase(json, showGameName);
 		})
 		.fail(function( jqxhr, textStatus, error ) {
 			var err = textStatus + ", " + error;
 			console.log( "Request Failed: " + err );
 			base.outputAddLines("Failed to Load json");
 			
-			base.internal_showRequest();
+			base.#showRequest();
 		});
 	}
 
-	loadDatabaseFromObject(gamedatabaseJSON, showGameName = true){
+	loadDatabaseFromObject(json, showGameName = true){
 		this.outputClear();
 		this.outputAddLines("Initializing Text Adventure Engine...");
-		base.internal_initDatbase(json, showGameName);
+		base.#initDatbase(json, showGameName);
 	}
 
 	input(cmd){
-		this.internal_praseCommand(cmd);
+		this.#praseCommand(cmd);
 	}
 
-	internal_initDatbase(gameDatabaseObject, showGameName = true){
-		this.internal_Database = gameDatabaseObject;
+	#initDatbase(gameDatabaseObject, showGameName = true){
+		this.#database = gameDatabaseObject;
 
 		if(this.TBA_DEBUG){
 			var base = this;
@@ -62,37 +57,37 @@ class textAdventureEngine {
 		}
 		if(showGameName){
 			this.outputClear();
-			this.outputAddLines("<br /><br />'"+this.internal_Database.general.title+"' by "+this.internal_Database.general.author+"<br>"+"Version: "+this.internal_Database.general.version+"<br />");
+			this.outputAddLines("<br /><br />'"+this.#database.general.title+"' by "+this.#database.general.author+"<br>"+"Version: "+this.#database.general.version+"<br />");
 		}else{
 			this.outputClear();
 		}
-		this.internal_praseCommand("welcome");
+		this.#praseCommand("welcome");
 	}
 	
-	internal_showRequest(){
-		this.outputAddLines(this.internal_Database.general.request);
+	#showRequest(){
+		this.outputAddLines(this.#database.general.request);
 	}
 	
-	internal_removeFromString(arr,str){
+	#removeFromString(arr,str){
 		let regex = new RegExp("\\b"+arr.join('|')+"\\b","gi");
 		let removed = str.replace(regex, '');
 		return removed.replace(/\s\s+/g, ' ');
 	}
 	
-	internal_praseCommand(cmd){
+	#praseCommand(cmd){
 		cmd = cmd.toLowerCase();
 	
 		// remove ignored words from command
-		cmd = this.internal_removeFromString(this.internal_Database.ignored_words, cmd);
+		cmd = this.#removeFromString(this.#database.ignored_words, cmd);
 		cmd = cmd.trim();
 		console.log("Stripped command of parser: '"+cmd+"'");
 	
 		if(cmd=="welcome"){
-			this.internal_Inventory = {};
-			if(this.internal_Database.general.start.text.length > 0){
-				this.outputAddLines(this.internal_Database.general.start.text);
+			this.#inventory = {};
+			if(this.#database.general.start.text.length > 0){
+				this.outputAddLines(this.#database.general.start.text);
 			}
-			this.internal_parseActionString(undefined, this.internal_Database.general.start.action);
+			this.#parseActionString(undefined, this.#database.general.start.action);
 		}else if(cmd == "debug"){
 			if(this.TBA_DEBUG==true){
 				this.TBA_DEBUG = true;
@@ -102,7 +97,7 @@ class textAdventureEngine {
 		}else if(cmd == "help" || cmd == "?" || cmd == "what" || cmd == "how" || cmd == "what do"){
 	
 			var allVerbs = "";
-			$.each( this.internal_Database.verbs, function( key, val ) {
+			$.each( this.#database.verbs, function( key, val ) {
 					if(allVerbs != ""){
 						allVerbs += ", "+val.name;
 					}else{
@@ -114,28 +109,28 @@ class textAdventureEngine {
 		} else {
 			let words = cmd.split(" ");
 	
-			let locationState = this.internal_getLocationState(this.internal_CurrentRoom);
-			let verb = this.internal_checkForVerb(words);
-			let object = this.internal_checkForObject(words);
+			let locationState = this.#getLocationState(this.#currentRoom);
+			let verb = this.#checkForVerb(words);
+			let object = this.#checkForObject(words);
 			let currentObjectState = undefined;
 			// Get current state of object if it's not the room / location
 			if(object != undefined){
-				currentObjectState = this.internal_getObjectState(object);
+				currentObjectState = this.#getObjectState(object);
 			}
 	
 			//no action
 			if(verb == undefined && object != undefined){
 				//this.outputAddLines("You want to do what with the "+currentObjectState.name+"?!");
 				this.outputAddLines("Unknown verb, please try to rephrase your command.");
-				this.internal_showRequest();
+				this.#showRequest();
 				return;
 			}
 	
 			// Look at room
 			if(verb != undefined && verb.name == "look" && (words.length === 1)){
-				locationState = this.internal_getLocationState(this.internal_CurrentRoom);
+				locationState = this.#getLocationState(this.#currentRoom);
 				this.writeLocationDescription(locationState.objects);
-				this.internal_showRequest();
+				this.#showRequest();
 				return;
 			}
 	
@@ -143,7 +138,7 @@ class textAdventureEngine {
 			if(verb != undefined && (object == undefined)){
 				console.log("object is undefined");
 				this.outputAddLines(verb.failed);
-				this.internal_showRequest();
+				this.#showRequest();
 				return;
 			}
 	
@@ -170,86 +165,86 @@ class textAdventureEngine {
 					if(objectStateVerbDefinition.action!=undefined){
 						if($.isArray(objectStateVerbDefinition.action) && objectStateVerbDefinition.action.length > 0) {
 							for(var i=0; i<objectStateVerbDefinition.action.length; i++){
-								this.internal_parseActionString(object, objectStateVerbDefinition.action[i]);
+								this.#parseActionString(object, objectStateVerbDefinition.action[i]);
 							}
 						}else{
-							this.internal_parseActionString(object, objectStateVerbDefinition.action);
+							this.#parseActionString(object, objectStateVerbDefinition.action);
 						}
 					}
 				}else{
 					this.outputAddLines("Sorry you can't do this with "+currentObjectState.name+".");
 				}
-				this.internal_showRequest();
+				this.#showRequest();
 				return;
 			}
 			this.outputAddLines("Please try to rephrase your command.");
 	
 		}
-		this.internal_showRequest();
+		this.#showRequest();
 	}
 	writeLocationDescription(objectsInLocation){
 		var fullLocationDescription = "";
 		for(var i =0; i< objectsInLocation.length; i++){
-			let thisObject = this.internal_Database.objects[objectsInLocation[i]];
+			let thisObject = this.#database.objects[objectsInLocation[i]];
 			fullLocationDescription += " "+(thisObject.states[thisObject.currentState].locationDescription);
 		}
 		this.outputAddLines(fullLocationDescription);
 		
-		if(Object.keys(this.internal_Inventory).length > 0){
-			for(var index in this.internal_Inventory) {
-				let currentItemDescription = this.internal_Inventory[index].states[this.internal_Inventory[index].currentState].locationDescription;
+		if(Object.keys(this.#inventory).length > 0){
+			for(var index in this.#inventory) {
+				let currentItemDescription = this.#inventory[index].states[this.#inventory[index].currentState].locationDescription;
 				if(currentItemDescription.length > 0){
 					this.outputAddLines(currentItemDescription);
 				}
 			}
 		}
 	}
-	internal_parseActionString(affectedObject, actionString){
+	#parseActionString(affectedObject, actionString){
 		var acts = actionString.split(" ");
 		if(acts[0]=="objectState" && acts.length == 2){ // change this Object
 			console.log("changing ObjectState to "+acts[1]);
 			affectedObject.currentState = acts[1];
 		}else if(acts[0]=="objectState" && acts.length == 3){ // Reference other object
 			console.log("changing ObjectState of "+acts[1]+" to "+acts[2]);
-			this.internal_Database.objects[acts[1]].currentState = acts[2];
+			this.#database.objects[acts[1]].currentState = acts[2];
 		}else if(acts[0]=="objectRemoveFromLocation"){
 			console.log("removing Object from Location:"+acts[1]);
-			var index = this.internal_getLocationState(this.internal_CurrentRoom).objects.indexOf(acts[1]);
+			var index = this.#getLocationState(this.#currentRoom).objects.indexOf(acts[1]);
 			if (index > -1) {
-				this.internal_getLocationState(this.internal_CurrentRoom).objects.splice(index, 1);
+				this.#getLocationState(this.#currentRoom).objects.splice(index, 1);
 				console.log("Removed object with index: "+index);
 			}else{
 				console.log("Object not found in location: " + acts[1]);
 			}
 		}else if(acts[0]=="objectAddToLocation"){
 			console.log("adding Object to Location:"+acts[1]);
-			this.internal_getLocationState(this.internal_CurrentRoom).objects.push(acts[1]);
+			this.#getLocationState(this.#currentRoom).objects.push(acts[1]);
 		}else if(acts[0]=="gotoLocation"){
 			console.log("SIWTCHING LOCATION TO:"+acts[1]);
-			this.internal_CurrentRoom = acts[1];
-			var currentRoomState =  this.internal_getLocationState(this.internal_CurrentRoom);
+			this.#currentRoom = acts[1];
+			var currentRoomState =  this.#getLocationState(this.#currentRoom);
 			this.writeLocationDescription(currentRoomState.objects);
 		}else if(acts[0]=="showLocationDescription"){
-			var currentRoomState =  this.internal_getLocationState(this.internal_CurrentRoom);
+			var currentRoomState =  this.#getLocationState(this.#currentRoom);
 			this.writeLocationDescription(currentRoomState.objects);
 		}else if(acts[0]=="inventoryAdd"){
 			console.log("Add or replace inventory item: " + acts[1]);
-			this.internal_Inventory[acts[1]] = this.internal_Database.objects[acts[1]];
+			this.#inventory[acts[1]] = this.#database.objects[acts[1]];
 		}else if(acts[0]=="inventoryRemove"){
 			console.log("Remove inventory object, if it exists "+acts[1]);
-			delete this.internal_Inventory[acts[1]];
+			delete this.#inventory[acts[1]];
 		}
 
 		
 	}
 	
-	internal_getLocationState(location){
-		return this.internal_Database.locations[location];
+	#getLocationState(location){
+		return this.#database.locations[location];
 	}
 	
-	internal_getObjectState(object){
+	#getObjectState(object){
 		if (typeof object === 'string' || object instanceof String){
-			let objectRef = this.internal_Database.objects[object];
+			let objectRef = this.#database.objects[object];
 			return objectRef.states[objectRef.currentState];
 		}else{
 			return object.states[object.currentState];
@@ -258,36 +253,36 @@ class textAdventureEngine {
 	
 	
 	
-	internal_checkForVerb(words){
+	#checkForVerb(words){
 		let value = undefined;
 		for(var i=0; i<words.length && value === undefined; i++){
 			//console.log( "Checking: "+words[i] );
-			$.each( this.internal_Database.verbs, function( key, val ) {
+			$.each( this.#database.verbs, function( key, val ) {
 				//console.log( "Key: "+key+ " " + val.words[0] );
 				let test = $.inArray(words[i], val.words);
 				//console.log("test result: "+ test);
 				if(test >= 0){
 					value = val;
-					return; // exit $.each
+					return;
 				}
 			});
 		}
 		return value;
 	}
 	
-	internal_checkForObject(words){
-		var locationState = this.internal_getLocationState(this.internal_CurrentRoom);
+	#checkForObject(words){
+		var locationState = this.#getLocationState(this.#currentRoom);
 		let value = undefined;
 	
 		// Check room Items
 		for(var i=0; i<words.length; i++){
 			
 			// Check inventory item
-			if(Object.keys(this.internal_Inventory).length > 0){
-				for(var index in this.internal_Inventory) {
-					let test = $.inArray(words[i],  this.internal_getObjectState(this.internal_Inventory[index]).words);
+			if(Object.keys(this.#inventory).length > 0){
+				for(var index in this.#inventory) {
+					let test = $.inArray(words[i],  this.#getObjectState(this.#inventory[index]).words);
 					if(test >= 0){
-						value = this.internal_Inventory[index];
+						value = this.#inventory[index];
 						return value;
 					}
 				}
@@ -295,9 +290,9 @@ class textAdventureEngine {
 			// check for objects in room
 			var base = this;
 			$.each(locationState.objects, function( index, val ) {
-				let test = $.inArray(words[i], base.internal_getObjectState(val).words);
+				let test = $.inArray(words[i], base.#getObjectState(val).words);
 				if(test >= 0){
-					value = base.internal_Database.objects[val];
+					value = base.#database.objects[val];
 					return; // exit $.each loop
 				}
 			});
@@ -309,8 +304,10 @@ class textAdventureEngine {
 	
 		return value;
 	}
-	internal_checkForSecondObject(words){
-		var locationState = this.internal_getLocationState(this.internal_CurrentRoom);
+
+	// disabled for now:
+	#checkForSecondObject(words){
+		var locationState = this.#getLocationState(this.#currentRoom);
 		console.log("checking for seconds object");
 		var value = undefined;
 		var founds = 0;
@@ -318,11 +315,11 @@ class textAdventureEngine {
 		for(var i=0; i<words.length; i++){
 			let isInventoryItem = false;
 			// Check inventory item
-			if(Object.keys(this.internal_Inventory).length > 0){
-				for(var index in this.internal_Inventory) {
-					let test = $.inArray(words[i],  this.internal_getObjectState(this.internal_Inventory[index]).words);
+			if(Object.keys(this.#inventory).length > 0){
+				for(var index in this.#inventory) {
+					let test = $.inArray(words[i],  this.#getObjectState(this.#inventory[index]).words);
 					if(test >= 0){
-						value = this.internal_Inventory[index];
+						value = this.#inventory[index];
 						founds++;
 						isInventoryItem = true;
 					}
@@ -332,9 +329,9 @@ class textAdventureEngine {
 				// check for objects in room
 				var base = this;
 				$.each(locationState.objects, function( index, val ) {
-					let test = $.inArray(words[i], base.internal_Database.objects[val].words);
+					let test = $.inArray(words[i], base.#database.objects[val].words);
 					if(test >= 0){
-						value = base.internal_Database.objects[val];
+						value = base.#database.objects[val];
 						founds++;
 						return;
 					}
@@ -347,10 +344,10 @@ class textAdventureEngine {
 		return value;
 	}
 	
-	internal_checkForObjectId(name){
+	#getObjectIndexByName(name){
 		console.log("checking for object id");
 		var value = undefined;
-		$.each(this.internal_Database.objects, function( index, val ) {
+		$.each(this.#database.objects, function( index, val ) {
 			if(val.states[val.currentState].name==name){
 				value = index;
 				return;
